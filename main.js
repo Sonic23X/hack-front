feb5fd446e57f7f59508f75bcb75b98c23391905
Alfredo Ortega <omar.alfredo49@gmail.com>
2019-12-08 10:45:33 -0600

    get poligons

--- main.js
+++ main.js
@@ -1,18 +1,70 @@
 $(document).ready(function ()
 {
     M.AutoInit();
-    var center = ol.proj.transform([-11011971.24, 2332451.05], 'EPSG:3857', 'EPSG:3857');
+    var styles = [
+      new ol.style.Style({
+        stroke: new ol.style.Stroke({
+          color: 'blue',
+          width: 3
+        }),
+        fill: new ol.style.Fill({
+          color: 'rgba(0, 0, 255, 0.1)'
+        })
+      }),
+      new ol.style.Style({
+        image: new ol.style.Circle({
+          radius: 0,
+          fill: new ol.style.Fill({
+            color: 'orange'
+          })
+        }),
+        geometry: function(feature) {
+          // return the coordinates of the first ring of the polygon
+          var coordinates = feature.getGeometry().getCoordinates()[0];
+          return new ol.geom.MultiPoint(coordinates);
+        }
+      })
+    ];
 
-  	var map = new ol.Map({
-  	   layers: [
-  		 new ol.layer.Tile({
-  			source: new ol.source.OSM()
-  		 })
-  	   ],
-  	   target: 'map',
-  	   view: new ol.View({
-    		 center: center,
-    		 zoom: 8
-  	   })
-  	});
+    var geojsonObject = {
+      'type': 'FeatureCollection',
+      'crs': {
+        'type': 'name',
+        'properties': {
+          'name': 'EPSG:3857'
+        }
+      },
+      'features': [{
+        'type': 'Feature',
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': [[[-5e6, 6e6], [-5e6, 8e6], [-3e6, 8e6],
+              [-3e6, 6e6], [-5e6, 6e6]], [[-5e6, 6e6], [-5e6, 8e6], [-3e6, 8e6],
+                  [-3e6, 6e6], [-5e6, 6e6]]]
+        }
+      }]
+    };
+
+    var source = new ol.source.Vector({
+      features: (new ol.format.GeoJSON()).readFeatures(geojsonObject)
+    });
+
+    var layer = new ol.layer.Vector({
+      source: source,
+      style: styles
+    });
+
+    var basic = new ol.layer.Tile({
+      source: new ol.source.OSM()
+    });
+
+    var map = new ol.Map({
+      layers: [basic, layer],
+      target: 'map',
+      view: new ol.View({
+        center: [0, 3000000],
+        zoom: 2
+      })
+    });
+
 });
